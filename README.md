174e795c1eaeb572268c9701eb4378c40b6f70c5
Christina <christina.e.c.barberi@gmail.com>
2023-02-23 15:27:10 -0500

    added init function to prompt users with questions. It takes the users input and generates a README file based on their answers

--- README.md
+++ README.md
@@ -1,3 +1,30 @@
-# Professional-README-Generator
-
-[How to create a Professiolan README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+# test
+  
+  ## Description
+  test
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+  test
+  
+  ## Usage
+  test
+  
+  ## Contributing
+  test
+  
+  ## Tests
+  test
+  
+  ## License
+  This project is licensed under the MIT license.
+  
+  ## Questions
+  If you have any questions, please feel free to reach out to me on GitHub (Christinaecb) or via email (christina.e.c.barberi@gmail.com).
--- index.js
+++ index.js
@@ -61,13 +61,60 @@ const questions = [
 // writeToFile function takes in a file name and data and uses Node.js 'fs' module to write data to the specified file
 // If there's an error it should be logged to the console
 function writeToFile(fileName, data) {
-    fs.writeFile(fileName, date, (err) =>
+    fs.writeFile(fileName, data, (err) =>
     err ? console.error(err) : console.log('README.md file created')
     );
 }
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+      const {
+        title,
+        description,
+        installation,
+        usage,
+        contributing,
+        tests,
+        license,
+        github,
+        email,
+      } = answers;
+  
+      const readmeTemplate = `# ${title}
+  
+  ## Description
+  ${description}
+  
+  ## Table of Contents
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [License](#license)
+  - [Questions](#questions)
+  
+  ## Installation
+  ${installation}
+  
+  ## Usage
+  ${usage}
+  
+  ## Contributing
+  ${contributing}
+  
+  ## Tests
+  ${tests}
+  
+  ## License
+  This project is licensed under the ${license} license.
+  
+  ## Questions
+  If you have any questions, please feel free to reach out to me on GitHub (${github}) or via email (${email}).`;
+  
+      writeToFile('README.md', readmeTemplate);
+    });
+  }
 
 // Function call to initialize app
 init();
